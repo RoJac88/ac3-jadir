f4df44ad1f8341aae0871ce6a3c1185257426fd2
RoJac88 <rodrigo.jacob@gmail.com>
2020-10-18 00:03:16 -0300

    Verificação de CEP

---
+++ app/static/cep.js
@@ -0,0 +1,16 @@
+const cepInput = document.querySelector('input[name="cep"]')
+cepInput.addEventListener('input', () => {
+  const cepDigits = cepInput.value.replace('-', '')
+  if (cepDigits.length === 8) {
+    const uri = `https://viacep.com.br/ws/${cepDigits}/json/`
+    fetch(uri)
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector('input[name="log"]').value = data.logradouro
+      document.querySelector('input[name="comp"]').value = data.complemento
+    })
+    .catch((error) => {
+      console.log('Erro ao buscar CEP:', error)
+    })
+  }
+})
