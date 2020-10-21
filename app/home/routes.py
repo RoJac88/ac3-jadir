c71aa67c0a11d799c1449cfd333c76e038cce36e
RoJac88 <rodrigo.jacob@gmail.com>
2020-10-21 17:38:42 -0300

    tratamento de erro e toque de estilo para firefox sem fontes

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from psycopg2.errors import UndefinedTable
 from dao import read_tablenames, read_table_columns, read_table_size
 from app.home import bp
 
@@ -9,8 +10,11 @@ def index():
     tabelas = read_tablenames()
     for tab in tabelas:
         meta = {'schemaname': tab['schemaname'], 'owner': tab['tableowner']}
-        size = int(read_table_size(tab['tablename'])['size'] / 1000)
-        meta['tamanho'] = f'{size:.2f} KB'
+        try:
+            size = int(read_table_size(tab['tablename'])['size'] / 1000)
+            meta['tamanho'] = f'{size:.2f} KB'
+        except UndefinedTable as e:
+            print(e)
         cols = []
         for col in read_table_columns(tab['tablename']):
             cols.append({'name': col['column_name'], 'type': col['data_type'], 'max_len': col.get('character_maximum_length')})
